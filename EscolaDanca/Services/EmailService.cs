26d4deb2fa001817e64503ebce5bd5b957d7f785
DevJoaoLoureiro <joaoloureirosk8@gmail.com>
2026-06-02 01:47:36 +0100

    fix email with brevo api

--- EscolaDanca/Services/EmailService.cs
+++ EscolaDanca/Services/EmailService.cs
@@ -25,11 +25,19 @@ public class EmailService : IEmailService
         message.From.Add(MailboxAddress.Parse(from));
         message.To.Add(MailboxAddress.Parse(to));
         message.Subject = subject;
-        message.Body = new TextPart("html") { Text = body };
+        message.Body = new TextPart("html")
+        {
+            Text = body
+        };
 
         using var client = new SmtpClient();
 
-        await client.ConnectAsync(smtpHost, smtpPort, SecureSocketOptions.SslOnConnect);
+        await client.ConnectAsync(
+            smtpHost,
+            smtpPort,
+            SecureSocketOptions.StartTls
+        );
+
         await client.AuthenticateAsync(smtpUser, smtpPass);
         await client.SendAsync(message);
         await client.DisconnectAsync(true);
